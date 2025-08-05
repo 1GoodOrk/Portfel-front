d6c154b667a10edc6c56caf7b1de049ef34624b5
1GoodOrk <eldarsalat@protonmail.com>
2025-08-05 17:11:42 +0000

    ui changes and translation for contacts, about

--- src/app/components/main/contacts/contacts.component.ts
+++ src/app/components/main/contacts/contacts.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
-
-import { HeaderComponent } from '../../shared/organisms/header/header.component';
-import { FooterComponent } from '../../shared/organisms/footer/footer.component';
-
+import { TranslatePipe } from "@ngx-translate/core";
 import { FormsModule } from '@angular/forms';
+
+
 import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import { MessageModule  } from 'primeng/message';
 import { ButtonModule } from 'primeng/button';
-import { HttpService } from '../../../services/http.service';
+import { CardModule } from 'primeng/card';
+import { TooltipModule } from 'primeng/tooltip';
+
+import { HttpService } from '@port/services/http.service';
+
+import { HeaderComponent } from '@port/shared/organisms/header/header.component';
+import { FooterComponent } from '@port/shared/organisms/footer/footer.component';
 
 @Component({
   selector: 'app-contacts',
-  imports: [HeaderComponent, FooterComponent, FormsModule, InputTextModule, ButtonModule],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    FormsModule,
+    InputTextModule,
+    TextareaModule,
+    MessageModule,
+    ButtonModule,
+    CardModule,
+    TooltipModule,
+    TranslatePipe
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
@@ -18,17 +36,19 @@ export class ContactsComponent {
   public formData: any = {
     email: '',
     theme: '',
-    comment: ''
+    message: ''
   }
 
   constructor (private httpService: HttpService) {}
 
-  public sendMessage () {
-    this.httpService.sendMessage(this.formData)
-    this.formData = {
-      email: '',
-      theme: '',
-      comment: ''
+  public onSubmit (form: any) {
+    if (form.valid) {
+      this.httpService.sendMessage(this.formData)
+      this.formData = {
+        email: '',
+        theme: '',
+        message: ''
+      }
     }
   }
 }
